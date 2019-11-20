0b554ff9355a6f87d90bf82da9d41a82818c684e
Nishtha3512 <51858166+Nishtha3512@users.noreply.github.com>
2019-11-21 00:36:46 +0530

    Add files via upload

---
+++ Display.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package package1;
+
+/**
+ *
+ * @author HP
+ */
+public class Display {
+
+   private String question;
+   private String Option1;
+   private String Option2;
+   private String Option3;
+   private String Option4;
+   private String Correct;
+   private String count;
+    private String Count;
+   
+   public String getQuestion()
+   {
+       return question;
+   }
+       public void setQuestion(String question)
+       {
+           this.question=question;
+       }
+        public String getOption1()
+   {
+       return Option1;
+   }
+       public void setOption1(String Option1)
+       {
+           this.Option1=Option1;
+       }
+       
+      public String getOption2()
+   {
+       return Option2;
+   }
+       public void setOption2(String Option2)
+       {
+           this.Option2=Option2;
+       }
+          public String getOption3()
+   {
+       return Option3;
+   }
+       public void setOption3(String Option3)
+       {
+           this.Option3=Option3;
+       }
+          public String getOption4()
+   {
+       return Option4;
+   }
+       public void setOption4(String Option4)
+       {
+           this.Option4=Option4;
+       }
+        public String getCorrect()
+   {
+       return Correct;
+      // return Correct;
+   }
+       public void setCorrect(String Correct)
+       {
+           this.Correct=Correct;
+       }
+        public String getCount()
+   {
+       return Count;
+   }
+       public void setCount(String Count)
+       {
+           this.Count=Count;
+       }
+       
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
